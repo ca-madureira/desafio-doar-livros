6a6d6884d5e5eda3b4e0327ce60c9e54034c5802
ca-madureira <ca.madureira094@gmail.com>
2025-04-02 22:53:40 -0300

    add axios get and axios post

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,19 +1,35 @@
 import livroProtagonista from '../../assets/livroProtagonista.png';
 import s from './livrosDoados.module.scss';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function LivrosDoados() {
+
+  const [livros, setLivros] = useState([])
+
+  const getLivros = async () => {
+    const resposta = await axios.get("https://api-python-6884.onrender.com/livros")
+    setLivros(resposta.data)
+  }
+
+  useEffect(() => {
+    getLivros()
+  }, [])
+
   return (
     <section className={s.livrosDoadosSection}>
       <h2>Livros Doados</h2>
       <section className={s.containerCards}>
-        <section>
-          <img src={livroProtagonista} alt="ilustração da capa do livro" />
-          <div>
-            <h3>O protagonista</h3>
-            <p>Susanne Andrade</p>
-            <p>Ficção</p>
-          </div>
-        </section>
+        {
+          livros.map((item, index) => (
+            <section key={index}>
+              <img src={item.imagem_url} alt={`Titulo do livro ${item.titulo}`} />
+              <h3>{item.titulo}</h3>
+              <p>{item.autor}</p>
+              <p>{item.categoria}</p>
+            </section>
+          ))
+        }
       </section>
     </section>
   );
--- src/pages/queroDoar/QueroDoar.jsx
+++ src/pages/queroDoar/QueroDoar.jsx
@@ -1,14 +1,47 @@
 import iconeLivroForm from '../../assets/iconeLivroForm.png';
 import s from './queroDoar.module.scss';
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function QueroDoar() {
+  const [titulo, setTitulo] = useState("")
+  const [categoria, setCategoria] = useState("")
+  const [autor, setAutor] = useState("")
+  const [imagem_url, setImagem_url] = useState("")
+
+  const capturaTitulo = (e) => {
+    setTitulo(e.target.value)
+  }
+
+  const capturaCategoria = (e) => {
+    setCategoria(e.target.value)
+  }
+
+  const capturaAutor = (e) => {
+    setAutor(e.target.value)
+  }
+
+  const capturaImagem = (e) => {
+    setImagem_url(e.target.value)
+  }
+
+  const enviaDados = async () => {
+    const dadosParaEnviar = {
+      titulo,
+      categoria,
+      autor,
+      imagem_url
+    }
+    await axios.post("https://api-python-6884.onrender.com/doar", dadosParaEnviar)
+  }
+
   return (
     <section className={s.queroDoarSection}>
       <p>
         Por favor, preencha o formulário com suas informações e as informações
         do Livro
       </p>
-      <form action="">
+      <form onSubmit={(e) => e.preventDefault()}>
         <div>
           <img
             src={iconeLivroForm}
@@ -16,11 +49,11 @@ export default function QueroDoar() {
           />
           <h2>Informações do Livro</h2>
         </div>
-        <input type="text" name="" id="" placeholder="Titulo" />
-        <input type="text" name="" id="" placeholder="Categoria" />
-        <input type="text" name="" id="" placeholder="Autor" />
-        <input type="text" name="" id="" placeholder="Link da Imagem" />
-        <button type="submit" className={s.buttonDoar}>
+        <input type="text" name="" id="" placeholder="Titulo" onChange={capturaTitulo} />
+        <input type="text" name="" id="" placeholder="Categoria" onChange={capturaCategoria} />
+        <input type="text" name="" id="" placeholder="Autor" onChange={capturaAutor} />
+        <input type="text" name="" id="" placeholder="Link da Imagem" onChange={capturaImagem} />
+        <button type="submit" className={s.buttonDoar} onClick={enviaDados}>
           Doar
         </button>
       </form>
